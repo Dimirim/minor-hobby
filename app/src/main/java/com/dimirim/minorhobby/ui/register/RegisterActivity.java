b8ce65d1aace0901bae0e7ab158fe1c5c5ebb93c
wkdwhddn06 <wkdwhddn06@naver.com>
2020-03-11 13:02:12 +0900

    Ui 패키지 구분, Activity 파일 전체 생성

---
+++ app/src/main/java/com/dimirim/minorhobby/ui/register/RegisterActivity.java
@@ -0,0 +1,16 @@
+package com.dimirim.minorhobby.ui.register;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.dimirim.minorhobby.R;
+
+public class RegisterActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+    }
+}
