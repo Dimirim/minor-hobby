b3e4582311361e3ff4b114a31e330b6205b275a0
Sookhee <s2018s38@e-mirim.hs.kr>
2020-04-11 05:03:15 +0900

    HobbyDetailActivity 검색 필터 팝업 구현

--- app/src/main/java/com/dimirim/minorhobby/ui/hobby_detail/HobbyDetailActivity.java
+++ app/src/main/java/com/dimirim/minorhobby/ui/hobby_detail/HobbyDetailActivity.java
@@ -1,16 +1,43 @@
 package com.dimirim.minorhobby.ui.hobby_detail;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.CompoundButton;
+import android.widget.PopupMenu;
+import android.widget.Switch;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
 
 import com.dimirim.minorhobby.R;
+import com.dimirim.minorhobby.ui.main.MainActivity;
+
+import java.util.ArrayList;
 
 public class HobbyDetailActivity extends AppCompatActivity {
+    HobbyPopupWindow popupWindow;
+    TextView searchView;
+    private boolean isSearchAll;
+    private ArrayList<String> tagList = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_hobby_detail);
+
+        searchView = (TextView) findViewById(R.id.tagShowTextView);
+        popupWindow = new HobbyPopupWindow(HobbyDetailActivity.this);
+        ConstraintLayout hobbySearch = (ConstraintLayout) findViewById(R.id.hobbySearchLayout);
+        hobbySearch.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                popupWindow.callFunction(searchView);
+//                isSearchAll = popupWindow.isSearchAll;
+            }
+        });
     }
+
 }
---
+++ app/src/main/java/com/dimirim/minorhobby/ui/hobby_detail/HobbyPopupWindow.java
@@ -0,0 +1,152 @@
+package com.dimirim.minorhobby.ui.hobby_detail;
+
+import android.app.Dialog;
+import android.content.Context;
+import android.content.res.ColorStateList;
+import android.graphics.Color;
+import android.graphics.Typeface;
+import android.graphics.drawable.ColorDrawable;
+import android.util.DisplayMetrics;
+import android.view.Gravity;
+import android.view.View;
+import android.view.Window;
+import android.widget.Button;
+import android.widget.CompoundButton;
+import android.widget.Switch;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.core.content.res.ResourcesCompat;
+
+import com.dimirim.minorhobby.R;
+import com.google.android.material.chip.Chip;
+import com.google.android.material.chip.ChipDrawable;
+import com.google.android.material.chip.ChipGroup;
+
+import java.util.ArrayList;
+
+public class HobbyPopupWindow {
+    Switch searchOption;
+    protected String searchText = "";
+    protected boolean isSearchAll = false;
+    protected ArrayList<String> returnTagList = new ArrayList<>();
+    private ArrayList<Boolean> isTagChecked = new ArrayList<>();
+    private ArrayList<String> tagList = new ArrayList<>();
+    private Context context;
+
+    public HobbyPopupWindow(Context context) {
+        this.context = context;
+    }
+
+    public void callFunction(final TextView searchView){
+
+        resetPopupWindow();
+
+        final Dialog filterPopupWindow = new Dialog(context);
+        filterPopupWindow.show();
+
+        context.setTheme(R.style.MeterialComponents);
+        DisplayMetrics dm = context.getResources().getDisplayMetrics();
+        filterPopupWindow.requestWindowFeature(Window.FEATURE_NO_TITLE);
+        filterPopupWindow.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
+        filterPopupWindow.setContentView(R.layout.hobby_custom_popup_window);
+        filterPopupWindow.getWindow().setGravity(Gravity.RIGHT | Gravity.TOP);
+
+        Button submitBtn = (Button) filterPopupWindow.findViewById(R.id.popupSubmitBtn);
+        submitBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                searchView.setText(getSearchbarText());
+                filterPopupWindow.dismiss();
+            }
+        });
+
+        Button cancleBtn = (Button) filterPopupWindow.findViewById(R.id.popupCancleBtn);
+        cancleBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                filterPopupWindow.dismiss();
+            }
+        });
+
+        searchOption = (Switch) filterPopupWindow.findViewById(R.id.searchOption);
+        searchOption.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                if(isChecked){
+                    isSearchAll = true;
+                }
+                else{
+                    isSearchAll = false;
+                }
+            }
+        });
+
+        ChipGroup mChipGroup = (ChipGroup) filterPopupWindow.findViewById(R.id.chipGroup);
+        for(int i = 0; i < tagList.size(); i++){
+            final Chip chip = new Chip(context);
+
+            //다중 선택 설정
+            chip.setChipDrawable(ChipDrawable.createFromAttributes(context, null, 0, R.style.Widget_MaterialComponents_Chip_Filter));
+            //체그 아이콘 없애기
+            chip.setCheckedIconVisible(false);
+            chip.setText("#"+tagList.get(i));
+            isTagChecked.add(false);
+            chip.setTag(i);
+            chip.setRippleColor(null);
+            chip.setChipStrokeWidth((float)(0.5*dm.density));
+            chip.setTypeface(ResourcesCompat.getFont(context, R.font.notosanscjkkrmedium));
+            chip.setChipStrokeColor(ColorStateList.valueOf(Color.parseColor("#999999")));
+            chip.setChipBackgroundColor(ColorStateList.valueOf(Color.parseColor("#ffffff")));
+            chip.setTextColor(ColorStateList.valueOf(Color.parseColor("#999999")));
+            chip.setOnClickListener(new Chip.OnClickListener(){
+                @Override
+                public void onClick(View v){
+                    if(!isTagChecked.get((int)(v.getTag()))){
+                        isTagChecked.set((int)(v.getTag()), true);
+                        chip.setChipStrokeColor(ColorStateList.valueOf(Color.parseColor("#9d3cc8")));
+                        chip.setTextColor(ColorStateList.valueOf(Color.parseColor("#9d3cc8")));
+                    }
+                    else{
+                        chip.setChipStrokeColor(ColorStateList.valueOf(Color.parseColor("#999999")));
+                        chip.setTextColor(ColorStateList.valueOf(Color.parseColor("#999999")));
+                        isTagChecked.set((int)(v.getTag()), false);
+                    }
+                }
+            });
+            mChipGroup.addView(chip);
+        }
+    }
+
+    public String getSearchbarText(){
+        for(int i = 0; i < tagList.size(); i++){
+            if(isTagChecked.get(i)) {
+                searchText = searchText.concat("#");
+                searchText = searchText.concat(tagList.get(i));
+                searchText = searchText.concat(", ");
+                returnTagList.add(tagList.get(i));
+            }
+        }
+
+        if(searchText.length() > 0){
+            searchText = searchText.substring(0, searchText.length()-2);
+        }
+        return searchText;
+    }
+
+    private void resetPopupWindow(){
+        searchText = "";
+        returnTagList.clear();
+        isTagChecked.clear();
+        tagList.clear();
+
+        //임시 태그 입니다
+        tagList.add("모임");
+        tagList.add("입문가이드");
+        tagList.add("강좌");
+        tagList.add("중고거래");
+        tagList.add("입문가이드");
+        tagList.add("질문");
+    }
+
+}
