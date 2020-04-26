d84450f2522e52141f113443383c913be1b3b9a8
wkdwhddn06 <wkdwhddn06@naver.com>
2020-04-26 16:48:47 +0900

    RegisterDetailActivity 추가

--- app/src/main/java/com/dimirim/minorhobby/ui/register/RegisterActivity.kt
+++ app/src/main/java/com/dimirim/minorhobby/ui/register/RegisterActivity.kt
@@ -1,12 +1,35 @@
 package com.dimirim.minorhobby.ui.register
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.dimirim.minorhobby.R
+import kotlinx.android.synthetic.main.activity_register.*
 
 class RegisterActivity : AppCompatActivity() {
+    private val REQUEST_IMAGE = 0
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_register)
+
+
+        editProfileBtn.setOnClickListener {
+            val intent = Intent().apply {
+                type = "image/*"
+                action = Intent.ACTION_GET_CONTENT
+            }
+            startActivityForResult(intent, REQUEST_IMAGE)
+        }
     }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        if (requestCode == REQUEST_IMAGE && resultCode == RESULT_OK && null != data) {
+            contentResolver.openInputStream(data.data!!)
+
+        }
+    }
+
 }
