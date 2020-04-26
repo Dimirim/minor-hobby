b6061398946426aa4c83d8934b82744fd031167d
wkdwhddn06 <wkdwhddn06@naver.com>
2020-04-26 16:04:07 +0900

    LoginActivity 작업

--- app/src/main/java/com/dimirim/minorhobby/ui/login/LoginActivity.kt
+++ app/src/main/java/com/dimirim/minorhobby/ui/login/LoginActivity.kt
@@ -10,9 +10,14 @@ class LoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+        val sharedPreferences = getSharedPreferences("User", 0)
+
+        if (!sharedPreferences.getBoolean("logged_in", false)) {
+            sharedPreferences.edit().putBoolean("logged_in", true).apply()
+        }
 
         googleLoginBtn.setOnClickListener {
-
+            //sharedPreferences.edit().putBoolean("logged_in", true).apply()
         }
     }
 
---
+++ app/src/main/java/com/dimirim/minorhobby/ui/splash/SplashActivity.kt
@@ -0,0 +1,22 @@
+package com.dimirim.minorhobby.ui.splash
+
+import android.content.Intent
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.dimirim.minorhobby.ui.login.LoginActivity
+import com.dimirim.minorhobby.ui.main.MainActivity
+
+class SplashActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        val sharedPreferences = getSharedPreferences("User", 0)
+
+        if (sharedPreferences.getBoolean("logged_in", false)) {
+            startActivity(Intent(this, MainActivity::class.java))
+        } else {
+            startActivity(Intent(this, LoginActivity::class.java))
+        }
+    }
+
+}
