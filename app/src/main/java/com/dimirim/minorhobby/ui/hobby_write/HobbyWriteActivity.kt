b17424985aedeae9819c7d6c7e21887ca4d2575c
SeungHyunMin <contact@seunghyun.in>
2020-04-27 00:21:21 +0900

    HobbyWriteActivity

--- app/src/main/java/com/dimirim/minorhobby/ui/hobby_write/HobbyWriteActivity.kt
+++ app/src/main/java/com/dimirim/minorhobby/ui/hobby_write/HobbyWriteActivity.kt
@@ -2,11 +2,33 @@ package com.dimirim.minorhobby.ui.hobby_write
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
 import com.dimirim.minorhobby.R
+import com.dimirim.minorhobby.databinding.ActivityHobbyWriteBinding
+import com.dimirim.minorhobby.databinding.ItemTagBinding
+import kotlinx.android.synthetic.main.activity_hobby_write.*
 
 class HobbyWriteActivity : AppCompatActivity() {
+    private lateinit var viewModel: HobbyWriteViewModel
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_hobby_write)
+        viewModel = ViewModelProvider(this).get(HobbyWriteViewModel::class.java)
+
+        val binding = DataBindingUtil
+            .setContentView<ActivityHobbyWriteBinding>(this, R.layout.activity_hobby_write)
+        binding.lifecycleOwner = this
+        binding.vm = viewModel
+
+        viewModel.appliedTags.observe(this, Observer {
+            it.forEach { tag ->
+                val view = DataBindingUtil.inflate<ItemTagBinding>(
+                    layoutInflater, R.layout.item_tag, tagsLayout, false)
+                view.tag = tag
+                tagsLayout.addView(view.root)
+            }
+        })
     }
 }
